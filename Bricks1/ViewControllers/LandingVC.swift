30ae1ce1cc3969764b446a57060446d322d61b75
ncernek <ncernek@gmail.com>
2019-04-06 08:28:32 -0600

    refactor landing vc

--- Bricks1/ViewControllers/LandingVC.swift
+++ Bricks1/ViewControllers/LandingVC.swift
@@ -4,7 +4,7 @@ import Firebase
 import Charts
 
 
-class LandingVC: UIViewController, UITableViewDataSource, StoreSubscriber, UITableViewDelegate, FetchDelegate {
+class LandingVC: UIViewController, StoreSubscriber, FetchDelegate, UITableViewDataSource {
     
     // FetchDelegate protocol
     func confirmation(title: String, message: String) {
@@ -156,76 +156,6 @@ class LandingVC: UIViewController, UITableViewDataSource, StoreSubscriber, UITab
     
     @IBAction func triggerGiveFeedbackAlert(_ sender: Any) {
         Alerts.giveFeedback(self, message: "How can I improve this app?")
-    }
-    
-    // TABLE VIEW
-    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "headerCell")! as! HeaderCell
-        cell.teamName.text = store.state.teams[section].name
-        cell.button.tag = section
-//        cell.button.addTarget(self, action: #selector(presentTeamVC), for: .touchUpInside)
-        cell.button.addTarget(self, action: #selector(inviteFriend), for: .touchUpInside)
-        
-        
-        return cell
-    }
-    
-    @objc func inviteFriend(_ sender: UIButton) {
-        let team = store.state.teams[sender.tag]
-        Alerts.inviteFriend(self, team: team)
-    }
-//    @objc func presentTeamVC(_ sender: UIButton) {
-//        if let teamVC = storyboard?.instantiateViewController(withIdentifier: "Team") as? TeamVC {
-//            teamVC.team = store.state.teams[sender.tag]
-//            navigationController?.pushViewController(teamVC, animated: true)
-//        }
-//    }
-    
-    
-    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return CGFloat(integerLiteral: 40)
-    }
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return store.state.teams.count
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        let team = store.state.teams[section]
-        return team.members.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "taskCell")! as! TaskCell
-        
-        let team = store.state.teams[indexPath.section]
-        let member = team.members[indexPath.row]
-
-        cell.username.text = member.username
-        cell.consistency.text = String(member.consistency)
-        cell.taskDescription.text = member.displayTask?.description ?? ""
-        
-        cell.tomorrowLabel.isHidden = true
-        if member.displayTask?.dueDate == naiveDate(delta: 1) { cell.tomorrowLabel.isHidden = false }
-        
-        // count unread messages
-        cell.grade.text = ""
-        cell.grade.isHidden = true
-        cell.username.font = UIFont.systemFont(ofSize: 15.0, weight: .medium)
-        if member.unreadMessageCount > 0 {
-            cell.grade.text = String(member.unreadMessageCount)
-            cell.grade.isHidden = false
-            cell.username.font = UIFont.systemFont(ofSize: 15.0, weight: .heavy)
-        }
-        
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let team = store.state.teams[indexPath.section]
-        let vc = ChatVC(currentUser: team.currentUser, threadOwner: team.members[indexPath.row], team: team)
-        navigationController?.pushViewController(vc, animated: true)
-        tableView.deselectRow(at: indexPath, animated: true)
     }
     
 
